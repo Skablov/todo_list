45fb8c20c680e708f931d015291b91caf46fcea4
Skablov <danilaskablov@list.ru>
2019-05-21 20:15:43 +0300

    add ejs file and first page index

---
+++ index.js
@@ -0,0 +1,25 @@
+const config = require("./config.js");
+const express = require("express");
+const MongoClient = require("mongodb").MongoClient;
+const mongoClient = new MongoClient(config.MONGO_URL, { useNewUrlParser: true }); // установка точки подключения
+const objectId = require("mongodb").ObjectID;
+const bodyParser = require("body-parser"); // Подключение необходимого стека
+
+
+
+const app = express(); // включаем express
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoClient.connect((err, client) => { //стандартное подключение к бд типа MongoDb
+  if(err) return console.log(err); // если ошибка, то все останавливаем и реторнем ошибку
+  dbClient = client;
+  app.locals.collection = client.db('todolist').collection('data'); // указыем имя кластера
+  app.listen(config.PORT, () => console.log(`Server start on ${config.PORT}`)) // начинаем слушать
+})
+
+app.get('/', (req, res) =>
+{
+  res.render('index');
+});
